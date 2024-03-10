4c25e1138d6b0ab487c88732695c3672e60b37fd
Casey Marshall <git@cmars.tech>
2024-03-10 11:31:43 -0500

    feat: concurrent verification of fetched pieces
    Track piece completion state and verify their digests as they are
    completed, requeuing them if invalid.

--- distrans-peer/src/fetcher.rs
+++ distrans-peer/src/fetcher.rs
@@ -1,13 +1,13 @@
-use std::cmp::min;
+use std::cmp::{min, Ordering};
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
 
 use distrans_fileindex::{Index, BLOCK_SIZE_BYTES, PIECE_SIZE_BLOCKS, PIECE_SIZE_BYTES};
 use flume::{unbounded, Receiver, Sender};
+use sha2::{Digest, Sha256};
 use tokio::fs::File;
-use tokio::io::{AsyncSeekExt, AsyncWriteExt, SeekFrom};
+use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt, SeekFrom};
 use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
 use tracing::{debug, instrument, warn};
@@ -103,16 +103,30 @@ impl Fetcher {
     }
 
     pub async fn fetch(self, cancel: CancellationToken) -> Result<()> {
-        let (sender, receiver) = unbounded();
+        let (fetch_block_sender, fetch_block_receiver) = unbounded();
+        let (verify_sender, verify_receiver) = unbounded();
 
         let index = self.index.clone();
         let mut tasks = JoinSet::new();
-        tasks.spawn(Self::enqueue_blocks(cancel.clone(), sender.clone(), index));
+        let done = CancellationToken::new();
+        tasks.spawn(Self::enqueue_blocks(
+            cancel.clone(),
+            fetch_block_sender.clone(),
+            index,
+        ));
+        tasks.spawn(self.clone().verify_blocks(
+            cancel.clone(),
+            done.clone(),
+            fetch_block_sender.clone(),
+            verify_receiver,
+        ));
         for i in 0..N_FETCHERS {
             tasks.spawn(self.clone().fetch_blocks(
                 cancel.clone(),
-                sender.clone(),
-                receiver.clone(),
+                done.clone(),
+                fetch_block_sender.clone(),
+                fetch_block_receiver.clone(),
+                verify_sender.clone(),
                 i,
             ));
         }
@@ -146,8 +160,10 @@ impl Fetcher {
     async fn fetch_blocks(
         mut self,
         cancel: CancellationToken,
-        sender: Sender<FileBlockFetch>,
-        receiver: Receiver<FileBlockFetch>,
+        done: CancellationToken,
+        fetch_block_sender: Sender<FileBlockFetch>,
+        fetch_block_receiver: Receiver<FileBlockFetch>,
+        verify_sender: Sender<PieceState>,
         task_id: u8,
     ) -> Result<()> {
         let mut fh_map: HashMap<usize, File> = HashMap::new();
@@ -157,15 +173,14 @@ impl Fetcher {
                 .api()
                 .import_remote_private_route(route_data)?,
         );
-        let mut heartbeat = tokio::time::interval(Duration::from_secs(1));
         loop {
             tokio::select! {
-                recv_fetch = receiver.recv_async() => {
+                recv_fetch = fetch_block_receiver.recv_async() => {
                     let fetch = match recv_fetch {
                         Ok(fetch) => fetch,
                         Err(e) => {
                             // This is practically impossible given we have a sender in scope here for retries...
-                            debug!(err = format!("{}", e), "all senders have been dropped");
+                            debug!(err = format!("{}", e), "all fetch block senders have been dropped");
                             return Ok(())
                         }
                     };
@@ -197,22 +212,16 @@ impl Fetcher {
 
                         let block_end = min(block.len(), BLOCK_SIZE_BYTES);
                         fh.write_all(block[fetch.piece_offset..block_end].as_mut()).await?;
-                        // TODO: update piece completion; we could verify
-                        // concurrently with fetching other pieces, reject &
-                        // requeue bad ones...
+                        verify_sender.send(PieceState::new(fetch.file_index, fetch.piece_index, fetch.piece_offset, fetch.block_index)).map_err(other_err)?;
                         Ok(())
                     }.await;
                     if let Err(e) = fetch_result {
                         warn!(err = format!("{}", e), "fetch block failed, queued for retry");
-                        sender.send(fetch).map_err(other_err)?;
+                        fetch_block_sender.send(fetch).map_err(other_err)?;
                     }
                 }
-                _ = heartbeat.tick() => {
-                    if receiver.is_empty() {
-                        debug!("all blocks have been fetched");
-                        // TODO: verify pieces!!!
-                        return Ok(())
-                    }
+                _ = done.cancelled() => {
+                    return Ok(())
                 }
                 _ = cancel.cancelled() => {
                     return Err(other_err("cancelled"))
@@ -294,6 +303,82 @@ impl Fetcher {
         }
         Ok(())
     }
+
+    async fn verify_blocks(
+        self,
+        cancel: CancellationToken,
+        done: CancellationToken,
+        fetch_block_sender: Sender<FileBlockFetch>,
+        verify_receiver: Receiver<PieceState>,
+    ) -> Result<()> {
+        let mut piece_states: HashMap<(usize, usize), PieceState> = HashMap::new();
+        let mut verified_pieces = 0;
+        loop {
+            tokio::select! {
+                recv_verify = verify_receiver.recv_async() => {
+                    // select on verify receiver
+                    let mut to_verify = match recv_verify {
+                        Ok(verify) => verify,
+                        Err(e) => {
+                            debug!(err = format!("{}", e), "all verify senders have been dropped");
+                            return Ok(())
+                        }
+                    };
+                    // update piece state
+                    if let Some(prior_state) = piece_states.get_mut(&to_verify.key()) {
+                        to_verify = prior_state.merged(to_verify);
+                        *prior_state = to_verify;
+                    } else {
+                        piece_states.insert(to_verify.key(), to_verify);
+                    }
+                    if to_verify.is_complete() {
+                        // verify complete ones
+                        if self.verify_piece(to_verify.file_index, to_verify.piece_index).await? {
+                            verified_pieces += 1;
+                            if verified_pieces == self.index.payload().pieces().len() {
+                                done.cancel();
+                                return Ok(())
+                            }
+                        } else {
+                            // re-queue fetch if invalid
+                            warn!(file_index = to_verify.file_index, piece_index = to_verify.piece_index, "invalid digest, retrying");
+                            for block_index in 0..32  {
+                                fetch_block_sender.send_async(FileBlockFetch{
+                                    file_index: to_verify.file_index,
+                                    piece_index: to_verify.piece_index,
+                                    piece_offset: to_verify.piece_offset,
+                                    block_index,
+                                }).await.map_err(other_err)?;
+                            }
+                        }
+                    }
+                }
+                _ = cancel.cancelled() => {
+                    return Err(other_err("cancelled"))
+                }
+            }
+        }
+    }
+
+    async fn verify_piece(&self, file_index: usize, piece_index: usize) -> Result<bool> {
+        let file_spec = &self.index.files()[file_index];
+        let mut fh = File::open(file_spec.path()).await?;
+        let piece_spec = &self.index.payload().pieces()[piece_index];
+
+        fh.seek(SeekFrom::Start((piece_index * PIECE_SIZE_BYTES) as u64))
+            .await?;
+        let mut buf = [0u8; BLOCK_SIZE_BYTES];
+        let mut digest = Sha256::new();
+        for _ in 0..PIECE_SIZE_BLOCKS {
+            let rd = fh.read(&mut buf[..]).await?;
+            if rd == 0 {
+                break;
+            }
+            digest.update(&buf[..rd]);
+        }
+        let actual_digest: [u8; 32] = digest.finalize().into();
+        Ok(piece_spec.digest().cmp(&actual_digest[..]) == Ordering::Equal)
+    }
 }
 
 #[derive(Debug)]
@@ -309,5 +394,45 @@ impl FileBlockFetch {
         (self.piece_index * PIECE_SIZE_BYTES)
             + self.piece_offset
             + (self.block_index * BLOCK_SIZE_BYTES)
+    }
+}
+
+#[derive(Clone, Copy)]
+struct PieceState {
+    file_index: usize,
+    piece_index: usize,
+    piece_offset: usize,
+    blocks: u32,
+}
+
+impl PieceState {
+    fn new(
+        file_index: usize,
+        piece_index: usize,
+        piece_offset: usize,
+        block_index: usize,
+    ) -> PieceState {
+        PieceState {
+            file_index,
+            piece_index,
+            piece_offset,
+            blocks: 1 << block_index,
+        }
+    }
+
+    fn key(&self) -> (usize, usize) {
+        (self.file_index, self.piece_index)
+    }
+
+    fn is_complete(&self) -> bool {
+        self.blocks == 0xffffffff
+    }
+
+    fn merged(&mut self, other: PieceState) -> PieceState {
+        if self.file_index != other.file_index || self.piece_index != other.piece_index {
+            panic!("attempt to merge mismatched pieces");
+        }
+        self.blocks |= other.blocks;
+        self.clone()
     }
 }
