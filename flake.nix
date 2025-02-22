4f6a90918cda2dd0cec2e4c56afd8eb7a14aa90a
Casey Marshall <git@cmars.tech>
2025-02-22 11:33:44 -0600

    fix: nix flake toolchain, cicd check
    Update nix flake to latest toolchain requirements; veilid-core needs a
    more recent rustc.
    
    Update flake deps and general cleanup; rust-overlay and crane have
    improved.
    
    Add a Github Action for CICD coverage of Nix flake.
    
    Fixes #223.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,27 @@
 {
-  description = "ddcp";
+  description = "distrans";
 
   inputs = {
-    # NixOS 23.11 has recent enough versions of capnproto and protobuf to
+    # NixOS 24.11 has recent enough versions of capnproto and protobuf to
     # develop on Veilid.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            rust-overlay.overlays.default
-          ];
+          inherit system overlays;
         };
-
+        craneLib = crane.mkLib pkgs;
         arch = flake-utils.lib.system.system;
 
       in {
@@ -34,7 +30,7 @@
           buildInputs = (with pkgs; [
             cargo
             cargo-watch
-            (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+            (rust-bin.stable."1.81.0".default.override { extensions = [ "rust-src" ]; })
             rustfmt
             rust-analyzer
             clang
@@ -52,12 +48,13 @@
           LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
         };
 
-        packages.default = crane.lib.${system}.buildPackage {
+        packages.default = craneLib.buildPackage {
+          pname = "distrans";
           src = ./.;
 
           buildInputs = with pkgs; [
             cargo
-            rust-bin.stable.latest.default
+            rust-bin.stable."1.81.0".default
             capnproto
             protobuf
             pkg-config
